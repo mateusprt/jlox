5437e694f9c5937b659163acd9998560502cf3c4
mateusprt <matnun95@gmail.com>
2024-04-22 22:43:35 -0300

    Add basic prompt

--- src/Lox.java
+++ src/Lox.java
@@ -1,5 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Lox {
-    public static void main(String[] args) {
-        System.out.println("Hello jlox");
+    public static void main(String[] args) throws IOException {
+        if(args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
     }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for(;;) {
+            System.out.print("\n> ");
+            String line = reader.readLine();
+            if(line == null) break;
+            run(line);
+        }
+    }
+
+    private static void run(String source) {
+        // TODO
+        System.out.printf("Token %s", source);
+    }
+
 }
