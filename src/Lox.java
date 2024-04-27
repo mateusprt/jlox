3cd2f2f739c095117b4d58bb8d287f1a9d0b97a9
mateusprt <matnun95@gmail.com>
2024-04-27 16:06:00 -0300

    Add error handling

--- src/Lox.java
+++ src/Lox.java
@@ -4,8 +4,12 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Lox {
+
+    static boolean hadError = false;
+
     public static void main(String[] args) throws IOException {
         if(args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -20,6 +24,7 @@ public class Lox {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+        if(hadError) System.exit(65);
     }
 
     private static void runPrompt() throws IOException {
@@ -31,12 +36,25 @@ public class Lox {
             String line = reader.readLine();
             if(line == null) break;
             run(line);
+            hadError = false;
         }
     }
 
     private static void run(String source) {
-        // TODO
-        System.out.printf("Token %s", source);
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for(Token token : tokens) {
+            System.out.println(token);
+        }
     }
 
+    static void error(int line,  String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
 }
