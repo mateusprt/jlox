06ed3dab3088bf4d5c47bc6270307db085656a55
mateusprt <matnun95@gmail.com>
2024-04-27 16:34:17 -0300

    Create the token class and token enum

--- src/Lox.java
+++ src/Lox.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 public class Lox {
